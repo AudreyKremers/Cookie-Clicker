548dfcea0adf1a7cd5b2f50bcb8a0d841af127bc
Pablo Mombo Rasero <One@MacBook-Pro-de-Pablo.local>
2019-02-07 16:55:43 +0100

    frist step

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,14 @@
 (function(){
+	var score = 0;
+	var valueClick = 1;
+	function afficheScore(){
+		document.getElementById("score").innerHTML = score;
+	}
+	document.getElementById("click").addEventListener("click", function() {
+		score = score + valueClick;
+		
+		afficheScore();
+    });
 	//your code here
 
 })();
