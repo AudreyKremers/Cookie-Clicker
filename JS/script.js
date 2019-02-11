f311dd1ad2c552e4de9ce257433a6c38d94282a2
Pablo Mombo Rasero <One@MacBook-Pro-de-Pablo.local>
2019-02-11 14:33:21 +0100

    Parte 2, 3, 13 et 14

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,7 @@
 (function(){
+
+	//Étape 2 et 3
+
 	var score = 0;
 	var valueClick = 1;
 	function afficheScore(){
@@ -8,7 +11,38 @@
 		score = score + valueClick;
 		
 		afficheScore();
+		disablerBonus();
     });
-	//your code here
+	
+	//Étape 14: désactiver les boutons (pour Bonus)
+
+	function disablerBonus(){
+		if (score >= 50) {
+			document.getElementById("bonus").removeAttribute("disabled");
+
+		} else {
+			document.getElementById("bonus").setAttribute("disabled","true");
+		}
+
+	}
+
+	// Étape 13: achat d'un bonus
+
+	document.getElementById("bonus").addEventListener("click", function() {
+		if (score >= 50) {
+			score = score - 50; afficheScore();
+			valueClick = valueClick * 2;
+			var counter = 10;
+			var timing = setInterval(function(){
+				console.log(counter);
+				counter--;
+				if(counter == 0){
+					valueClick = valueClick / 2;
+	        	clearInterval(timing);
+	        	}
+	    	}, 1000);
+		}
+    });
+    disablerBonus();
 
 })();
