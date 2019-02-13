d272ca16bd9509a17e91ccfe4ecc746889201075
BeCode <nowakperrykeanou@gmail.com>
2019-02-13 09:22:12 +0100

    first commit cookies

--- JS/script.js
+++ JS/script.js
@@ -1,73 +1,86 @@
 (function(){
+	var score = 0;
+	var Score = 20;
+	var scoreMutli = Score + multiplicateur;
+	var multiplicateur = 0;
+	var Multiplicateur = 1;
+	var prixMultiplicaeur = 50;
+	var prixAutoclick = 500;
+	var prixBonus = 600;
+	var autoClicker;
+	var seconds = 30;
+	var stockChekScore = score;
+	function acg(){
+			score+=Score;
+			document.getElementById('score').innerHTML = score;
+	};
 
-function AutoclickerAuto(){
-	var autoclickerScore = document.getElementById('score').addEventListener('click',function(){
-		var score = document.getElementById('score').value;
-		console.log(score);
-		while(score >= 200 && score <= 500){
+	var final = setInterval(function(){
+			if(score >= 200 && score < 500){
+					score+=Score;
+					console.log(score);
+					document.getElementById('score').innerHTML = score;
+			}
+			else if(score >= 500){
+					clearInterval(final);
+			}
+	},1000);
+
+	
+
+	document.getElementById('click').addEventListener('click',function(){
+			score+=Score;
+			document.getElementById('score').innerHTML = score;
+			console.log(score); 
 			
-			score++;
-		}
-		console.log(score);
-	})
-}
-
-function autoclickCinqCents(){
-	var autoCInqCents = document.getElementById('autoclickerCinqCent').addEventListener('click',function fnButtomClick(){
-		function clickbttm(){
-			var score = document.getElementById('score').value;
-			onClick=this.style.visibility='hidden';
 			
-		}
-	})
+	});
 
+
+
+	document.getElementById('multiplier').addEventListener("click",function(){
+					
+			if(score >= prixMultiplicaeur){
+					multiplicateur += Multiplicateur;
+					console.log(multiplicateur);
+					Score++;
+					score -= prixMultiplicaeur;
+					document.getElementById('score').innerHTML = score;
+					prixMultiplicaeur *= 2;
+					document.getElementsByClassName('nom')[0].innerHTML = "multiplicateur x " + multiplicateur + " prix : " + prixMultiplicaeur;
+			
+					}
+					
+							
+	});
+
+	document.getElementById('autoclicker').addEventListener("click",function(){
+			if(score >= prixAutoclick){
+					score-=prixAutoclick;
+					document.getElementById('score').innerHTML = score;
+					prixAutoclick+=500;    
+	function printTimeUntil10() {
+			
+			seconds--; 
+			console.log("Seconds elapsed: " + seconds);
+			score +=Score;
+			console.log(score);
+			document.getElementById('score').innerHTML = score;
+
+			if (seconds === 0) {
+					console.log("30 seconds has elapsed!")
+					clearInterval(interval); 
+			}
+	}
+
+	var interval = setInterval(printTimeUntil10, 1000);
+	document.getElementsByClassName('nom')[1].innerHTML = "autoclick " + prixAutoclick + " $";
+	
 }
+	
 
 
+})
 
-
-
-
-
-
-
+			
 })();
-
-
-/*
-
-
-/*
-			quand le score == 200, l'autoclick s'active jusqu'a 500 point et puis il faut
-			l'acheter. Il dure 1 minutes et il faudra le reacheter.Une fois qu'il est acheter,
-			il faut que le boutton soit desactiver pour lorsqu'il est acheter.
-
-
-			l'autoclick se lance seulement au score 200 une fois seulement lorsque l'utilisateur
-			click pour la premiere fois sur le coockies. presciser ca dans la fonction.
-			Ensuite, le score s'autoclick jusqu'a 500 et à partir de ce moment là, l'autoclick
-			s'arrete et il faut l'acheter pour que sa s'autoclick a nouveau.
-			Qand l'utilisateur click sur l'autoclick, ce bouton deviens noir ou transparent et ne reviens,
-			seulement lorsque le compteur/temps de la fonction et terminer. L'utilisateur peut le reutiliser
-			au temps de fois qu'il veux. Le temps du bouton vaudra 1 minutes.
-
-
-Étape 11 : autoclicker
-Nouvelle amélioration à acheter : Autoclick. Dès que vous avez un score de 200, 
-un clic est fait automatiquement chaque seconde. (setInterval est ton ami)
-
-Étape 12 : achat d'autoclicker
-L'autoclicker ne sera plus automatique. Il faut dorénavant acheter l'autoclicker.
- Vous ajouterez un bouton avec l'id autoclic. Il coutera 500. Une fois que l'autoclicker a été acheté
- , il faudra désactiver le bouton d'achat de l'autoclic.
-
-Étape 13 : achat d'un bonus
-Désormais, on doit pouvoir acheter un bonus qui augmente le score de 200% à chaque clic pendant 
-30 secondes. C'est à dire que si à chaque clic, le score augmente de 3, pendant les 30 secondes 
-de bonus le score augmentera de 6 à chaque clic, au bout des 30 secondes le score augmentera à 
-nouveau de 3 par clic. Vous créerez le bouton « BONUS » avec l'id bonus, et le bonus coutera 5000.
- Lorsque le bonus est acheté, le nombre de seconde s'affichera dans le bouton avec le décompte et 
- le bouton devra etre désactivé pendant le temps du bonus. Quand le bonus est fini, le bouton est 
- réactivé et retrouve son texte d'origine.
-
-*/
